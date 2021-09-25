7b5c27642c2da68a5450da2dd9ea0ede1ccadde3
arcibashev71 <arcibashev71@yandex.ru>
2021-09-25 18:00:35 +0500

    refactor(kinopoiskAPI.HTTPRequest.TokenReader.TokenReader): в методе ReadToken разделены ошибки чтения токена

--- src/kinopoiskAPI/HTTPRequest/TokenReader/TokenReader.java
+++ src/kinopoiskAPI/HTTPRequest/TokenReader/TokenReader.java
@@ -12,9 +12,17 @@ public class TokenReader {
     public static String ReadToken() {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(pathToToken))) {
             return bufferedReader.readLine();
-        } catch (IOException e) {
-            System.out.println("The token was not found");
+        } catch (FileNotFoundException e) {
+            System.out.println(
+                    """
+                            The token was not found
+                            The token can be obtained on the website https://kinopoiskapiunofficial.tech/
+                            Copy the obtained token to a file 'token', then place this file in the
+                            src/kinopoiskAPI/HttpRequest/TokenReader/token directory""");
             System.exit(1);
+        } catch (IOException e) {
+            System.out.println("The token could not be read");
+            System.exit(2);
         }
         return null;
     }
