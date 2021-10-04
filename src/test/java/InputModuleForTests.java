bb1101816ec24f3e812039f7d242027ea86cf3bc
arcibashev71 <arcibashev71@yandex.ru>
2021-10-04 15:44:01 +0500

    Создан класс TokenizerTests для тестирования класса Tokenizer. Для этого также создан вспомогательный класс InputModuleForTests

---
+++ src/test/java/InputModuleForTests.java
@@ -0,0 +1,24 @@
+import inputModule.InputModule;
+import inputModule.Lexeme;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class InputModuleForTests implements InputModule {
+    private ListIterator<Lexeme> iterator;
+
+    public InputModuleForTests(List<Lexeme> lexemes) {
+        setNewLexemes(lexemes);
+    }
+
+    public void setNewLexemes(List<Lexeme> lexemes) {
+        this.iterator = lexemes.listIterator();
+    }
+
+    @Override
+    public Lexeme getNextLexeme() {
+        if (!iterator.hasNext())
+            return null;
+        return iterator.next();
+    }
+}
