6993d558723a84e0fa6c9937b9fde88537098fa0
arcibashev71 <arcibashev71@yandex.ru>
2021-10-10 14:18:30 +0500

    Небольшой фикс в ConsoleOutput

--- src/main/java/dataIO/consoleOutput/ConsoleOutput.java
+++ src/main/java/dataIO/consoleOutput/ConsoleOutput.java
@@ -8,7 +8,7 @@ public class ConsoleOutput implements OutputModule {
     public void sendMessage(String message, String userId) {
         for (int i = 0; i < 20; i++)
             System.out.print('-');
-        System.out.printf("\n>> USERID: %s\n>> MESSAGE:\n%s", userId, message);
+        System.out.printf("\n>> USERID: %s\n>> MESSAGE:\n%s\n", userId, message);
         for (int i = 0; i < 20; i++)
             System.out.print('-');
         System.out.println('\n');
